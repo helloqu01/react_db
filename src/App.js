34beb986522dd09681db4e1d5e2ff2ca6b84e27c
ohhuynji <helloqu@naver.com>
2022-05-04 15:46:35 +0900

    db연결

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import Axios from 'axios';
+import {React, useEffect} from 'react';
 function App() {
+
+  useEffect(()=>{
+    Axios.get('http://localhost:3001/api/get').then((Response)=>{
+      console.log(Response.data)
+    })
+
+    
+  }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      test
     </div>
   );
 }
